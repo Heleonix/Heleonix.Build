717a60f47be8d3b2cd9c40c19dd7939870d9fc2a
Hennadii Lutsyshyn <hennadii.lutsyshyn@gmail.com>
2023-03-26 00:57:43 +0200

    Restored NetSimulator in git.

---
+++ test/Heleonix.Build.Tests/NetSimulator/NetSimulator.Tests/FullyCoveredTypeTests.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="FullyCoveredTypeTests.cs" company="Heleonix - Hennadii Lutsyshyn">
+// Copyright (c) Heleonix - Hennadii Lutsyshyn. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the repository root for full license information.
+// </copyright>
+
+namespace NetSimulator
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests the <see cref="FullyCoveredType"/>.
+    /// </summary>
+    public static class FullyCoveredTypeTests
+    {
+        /// <summary>
+        /// Tests the <see cref="FullyCoveredType.PlusOne"/>.
+        /// </summary>
+        [Test]
+#pragma warning disable S2699 // Tests should include assertions
+        public static void PlusOne()
+#pragma warning restore S2699 // Tests should include assertions
+        {
+            FullyCoveredType.PlusOne(12);
+        }
+    }
+}
