275307fe1fa05a8873c4c2367aea3aacd9b5f8de
Hennadii Lutsyshyn <hennadii.lutsyshyn@gmail.com>
2023-03-27 22:19:47 +0300

    Fixed tests to install packages at run time to reduce time of uploading artifacts on CI.

---
+++ test/Heleonix.Build.Tests/Module.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="Module.cs" company="Heleonix - Hennadii Lutsyshyn">
+// Copyright (c) Heleonix - Hennadii Lutsyshyn. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the repository root for full license information.
+// </copyright>
+
+namespace Heleonix.Build.Tests;
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Heleonix.Build.Tests.Common;
+
+/// <summary>
+/// Initializes the assembly.
+/// </summary>
+internal static class Module
+{
+    /// <summary>
+    /// Initializes the assembly.
+    /// </summary>
+    [ModuleInitializer]
+    internal static void Initialize()
+    {
+        Directory.CreateDirectory(PathHelper.HeleonixBuildDir);
+
+        var args = ArgsBuilder.By("-", " ")
+            .AddValue("install")
+            .AddValue("Heleonix.Build")
+            .AddArgument("Version", FileVersionInfo.GetVersionInfo(typeof(PathHelper).Assembly.Location).ProductVersion)
+            .AddPath("Source", PathHelper.CurrentDir);
+
+        ExeHelper.Execute(PathHelper.NugetExe, args, true, PathHelper.HeleonixBuildDir);
+    }
+}
